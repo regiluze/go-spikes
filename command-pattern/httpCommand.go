db889ce3c4a9c9cdbff3d0a6cb0cde90615b9d69
regiluze <regiluze@hotmail.com>
2015-02-01 01:25:29 +0100

    httpCommand.go file extracted

---
+++ command-pattern/httpCommand.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type CommandFactory interface {
+	Get(string) Command
+}
+
+type Data struct {
+	Msg string
+}
+
+type CommandHandler struct {
+	CommandChannel chan Command
+	Factory        CommandFactory
+}
+
+func NewCommandHandler(cf CommandFactory) *CommandHandler {
+
+	ch := &CommandHandler{CommandChannel: make(chan Command),
+		Factory: cf}
+	return ch
+
+}
+
+func (ch *CommandHandler) handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "PUT" {
+		decoder := json.NewDecoder(r.Body)
+		var d Data
+		err := decoder.Decode(&d)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(d)
+		c := ch.Factory.Get(d.Msg)
+		ch.CommandChannel <- c
+	}
+}
--- command-pattern/server.go
+++ command-pattern/server.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
@@ -13,41 +11,6 @@ func NewServer() *Server {
 	s := &Server{}
 	return s
 
-}
-
-type CommandFactory interface {
-	Get(string) Command
-}
-
-type Data struct {
-	Msg string
-}
-
-type CommandHandler struct {
-	CommandChannel chan Command
-	Factory        CommandFactory
-}
-
-func NewCommandHandler(cf CommandFactory) *CommandHandler {
-
-	ch := &CommandHandler{CommandChannel: make(chan Command),
-		Factory: cf}
-	return ch
-
-}
-
-func (ch *CommandHandler) handler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "PUT" {
-		decoder := json.NewDecoder(r.Body)
-		var d Data
-		err := decoder.Decode(&d)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(d)
-		c := ch.Factory.Get(d.Msg)
-		ch.CommandChannel <- c
-	}
 }
 
 func (s *Server) start(cf CommandFactory) <-chan Command {
