6cc01aeedb3819cbaea1839dd8823ab66512353a
regiluze <regiluze@hotmail.com>
2015-02-01 10:54:39 +0100

    view handle func added

--- http-server/main.go
+++ http-server/main.go
@@ -34,9 +34,15 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view?id="+t.Name()[6:], 302)
 }
 
+func view(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "image")
+	http.ServeFile(w, r, "image-"+r.FormValue("id"))
+}
+
 func main() {
 	fmt.Println("kaixo")
 	http.HandleFunc("/", handle)
+	http.HandleFunc("/view", view)
 	http.ListenAndServe(":8080", nil)
 	fmt.Println("agur")
 
