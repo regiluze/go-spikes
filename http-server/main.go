21b51587e9c24c89547c5d37a2b7bcf38a8809db
regiluze <regiluze@hotmail.com>
2015-02-01 12:24:17 +0100

    error handler func added

--- http-server/main.go
+++ http-server/main.go
@@ -5,12 +5,14 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"text/template"
 )
 
 var uploadTemplate = template.Must(template.ParseFiles("index.html"))
+var errorTemplate = template.Must(template.ParseFiles("error.html"))
 
-func handle(w http.ResponseWriter, r *http.Request) {
+func upload(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		uploadTemplate.Execute(w, nil)
 		return
@@ -39,11 +41,20 @@ func view(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "image-"+r.FormValue("id"))
 }
 
+func errorHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if recoverErr := recover(); recoverErr != nil {
+				fmt.Fprintf(os.Stderr, "were panicing: %s\n", recoverErr)
+				w.WriteHeader(500)
+				errorTemplate.Execute(w, recoverErr)
+			}
+		}()
+		fn(w, r)
+	}
+}
 func main() {
-	fmt.Println("kaixo")
-	http.HandleFunc("/", handle)
-	http.HandleFunc("/view", view)
+	http.HandleFunc("/", errorHandler(upload))
+	http.HandleFunc("/view", errorHandler(view))
 	http.ListenAndServe(":8080", nil)
-	fmt.Println("agur")
-
 }
