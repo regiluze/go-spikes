0f649f96cb5bac68d8ddc54f2bcbc9b17f32bec3
regiluze <regiluze@hotmail.com>
2015-02-07 11:56:48 +0100

    error template setted as parameter

--- httpserver/imageUpload/main.go
+++ httpserver/imageUpload/main.go
@@ -11,7 +11,7 @@ func main() {
 	port := flag.String("port", "8080", "listen port")
 	address := flag.String("address", "0.0.0.0", "server address")
 	flag.Parse()
-	uploadHandler := httpserver.NewImageUploadHandler()
+	uploadHandler := NewImageUploadHandler()
 	server := httpserver.NewHttpServer(uploadHandler, *address, *port)
 	server.SetErrTemplate(ErrorTemplate)
 	error := server.Start()
