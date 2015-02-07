7afca59cc2101513e085d6e5d24a93c06144c97f
regiluze <regiluze@hotmail.com>
2015-02-07 10:35:13 +0100

    bugfix

---
+++ httpserver/imageUpload/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/regiluze/go-spikes/httpserver"
+)
+
+func main() {
+	port := flag.String("port", "8080", "listen port")
+	address := flag.String("address", "0.0.0.0", "server address")
+	flag.Parse()
+	uploadHandler := httpserver.NewImageUploadHandler()
+	server := httpserver.NewHttpServer(uploadHandler, *address, *port)
+	error := server.Start()
+	if error != nil {
+		fmt.Println(error)
+	}
+
+}
