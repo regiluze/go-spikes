afe014c9cf0c4b337d9472d2bba613c5bd65798e
regiluze <regiluze@hotmail.com>
2015-02-07 11:51:16 +0100

    test

--- httpserver/server.go
+++ httpserver/server.go
@@ -47,9 +47,7 @@ func (s *HttpServer) errorHandler(fn http.HandlerFunc) http.HandlerFunc {
 			if recoverErr := recover(); recoverErr != nil {
 				error := NewError(fmt.Sprintf("\"%v\"", recoverErr))
 				w.WriteHeader(500)
-				if s.errTemplate {
-					errTemplate.Execute(w, error)
-				}
+				s.errTemplate.Execute(w, error)
 			}
 		}()
 		fn(w, r)
